ebc0626b9d0383713e0b806bde3b0cb7c9ecefc6
hexymouse <hexymouse@gmail.com>
2018-07-21 01:34:10 +0300

    start creating

--- src/MyArrayList/MyArrayList.java
+++ src/MyArrayList/MyArrayList.java
@@ -3,6 +3,16 @@ package MyArrayList;
 import java.util.AbstractList;
 
 public class MyArrayList<T> extends AbstractList {
+
+    private int size;
+    private static final int defaultSize = 10;
+    private int cap;
+    private T[] list;
+
+    public MyArrayList() {
+
+    }
+
     @Override
     public Object get(int i) {
         return null;

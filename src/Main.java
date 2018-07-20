c2c18d5285de536ba1098bb657bd6137ca838ba8
hexymouse <hexymouse@gmail.com>
2018-07-21 01:14:52 +0300

    add myArrayList class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
+import MyArrayList.MyArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        MyArrayList<String> list = new MyArrayList<String>();
+    }
 }
---
+++ src/MyArrayList/MyArrayList.java
@@ -0,0 +1,15 @@
+package MyArrayList;
+
+import java.util.AbstractList;
+
+public class MyArrayList<T> extends AbstractList {
+    @Override
+    public Object get(int i) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}

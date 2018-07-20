72debcc3002354eb73a73064c468b0a6d3e21ecf
hexymouse <hexymouse@gmail.com>
2018-07-21 02:22:06 +0300

    complete

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ import MyArrayList.MyArrayList;
 public class Main {
     public static void main(String[] args) {
         MyArrayList<String> list = new MyArrayList<String>();
+        list.put("one");
+        list.put("two");
+        list.put("three");
+        list.put("four");
+        System.out.println(list.showAll());
     }
 }
--- src/MyArrayList/MyArrayList.java
+++ src/MyArrayList/MyArrayList.java
@@ -1,25 +1,37 @@
 package MyArrayList;
 
 import java.util.AbstractList;
+import java.util.Arrays;
 
 public class MyArrayList<T> extends AbstractList {
 
-    private int size;
-    private static final int defaultSize = 10;
-    private int cap;
-    private T[] list;
-
-    public MyArrayList() {
-
-    }
+    private final int DEFAULT_SIZE = 3;
+    private int coef = 2;
+    private Object[] array = new Object[DEFAULT_SIZE];
+    private int index = 0;
 
     @Override
     public Object get(int i) {
-        return null;
+        return array[i];
+    }
+
+    public void put(T item) {
+        if (index == array.length) {
+            Object[] newArray = new Object[array.length * coef];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+        array[index++] = item;
     }
 
     @Override
     public int size() {
-        return 0;
+        return array.length;
+    }
+
+    public String showAll() {
+        return Arrays.toString(array);
     }
 }
